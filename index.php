c7f8a6819807c6a70c7eb92ce9209df5c76b039e
Tehnikumi Opilane <opilane@tptlive.ee>
2018-01-03 12:59:36 +0200

    Esimene commit, 03.01.2018, index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<?php
+// tehtud Atom-i nimelise tarkvaraga
+$greetings="Tere";
+// tekstimuutuja
+$nr = 1;
+// Täisarvuline muutuja
+$nr = 10.0231;
+// kümnendmurd
+$bool = False;
+//tõeväärtus
+$massiiv = array('Nimi',1974,10.12,'Perenimi');
+// masiivis algab number "0"-iga
+
+echo "$greetings tulemast!<br>";
+
+// echo '$greetings tulemast!';
+echo $massiiv [3];
+// kui tahan kuvada "Perenimi", siis masiivist number "3"
+
+/*
+nende vahele saab kommenteerida ohjeldamatult
+nagu näha tegin teise rea ka kohe järgi
+*/
+
+ ?>
