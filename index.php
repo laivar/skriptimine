feabdd806d07360ff9b3ceb45e702e6255bb1840
Tehnikumi Opilane <opilane@tptlive.ee>
2018-01-22 13:32:37 +0200

    Massiivi töötlemine 03

--- index.php
+++ index.php
@@ -12,7 +12,16 @@ $bool = False;
 $massiiv = array('Nimi',1974,10.12,'Perenimi');
 // masiivis algab number "0"-iga
 
-echo "$greetings tulemast!<br>";
+$esta = 'Eesti keel';
+$mata = 'Matemaatika';
+$fys = 'Füüsika';
+
+//sidusmassivid
+$hinded = array('Mati'=>array( $esta =>4, $mata =>4, $fys =>1),
+                'Kati'=>array( $esta =>3, $mata =>4, $fys =>5),
+                'Kaie'=>array( $esta =>5, $mata =>5, $fys =>5));
+
+echo "$greetings tulemast!".'<br>';
 
 // echo '$greetings tulemast!';
 // kui tahan kuvada "Perenimi", siis masiivist number "3"
@@ -26,8 +35,19 @@ nagu näha tegin teise rea ka kohe järgi
 if ($bool == False){
 // print_r($massiiv);
 //  print_r
-for ($i = 0; $i < count($massiv); $i++ ) {
+for ($i = 0; $i < count($massiiv); $i++ ) {
   echo $massiiv [$i]. '<br>';
 }
 } else { echo "Tõeväärtus ei vasta nõuetele";}
+
+$hinded_id = array_keys($hinded);
+for ($i=0; $i < count($hinded); $i++){
+  echo $hinded_id[$i].'<br>';
+  foreach ($hinded[$hinded_id[$i]] as $aine => $hinne) {
+    echo $aine." : ".$hinne.'<br>';
+
+
+    # code...
+  }
+}
  ?>
